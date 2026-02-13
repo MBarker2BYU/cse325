703fa6f4ba64340e0bf321cdd15c08ddb443b209
ShadowWorx <mdbarker813@att.net>
2026-02-12 22:33:43 -0500

    FixAttendanceSubmittedBool  updates

---
+++ ServePoint.Cadet/Migrations/20260213033234_FixAttendanceSubmittedBool.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServePoint.Cadet.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixAttendanceSubmittedBool : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            if (migrationBuilder.ActiveProvider == "Npgsql.EntityFrameworkCore.PostgreSQL")
+            {
+                migrationBuilder.Sql("""
+                                         ALTER TABLE "VolunteerSignups"
+                                         ALTER COLUMN "AttendanceSubmitted" TYPE boolean
+                                         USING (COALESCE("AttendanceSubmitted", 0) <> 0);
+                                     """);
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
